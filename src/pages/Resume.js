763197c1fa0f276ae60e635cc772d0486297ddfb
Donovan McGilbray <djaymcgilbray@gmail.com>
2026-03-12 16:37:21 -0700

    created resume page UI with CSS styling

---
+++ src/pages/Resume.js
@@ -0,0 +1,35 @@
+import "./Resume.css";
+
+function Resume() {
+    return (
+        <div className = "resume-container">
+            <h1>Resume</h1>
+            <section>
+                <h2>Professional Summary</h2>
+                <p>
+                    ...
+                </p>
+            </section>
+            <section>
+                <h2>Technical skills</h2>
+                <ul>
+                    <li>...</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Education</h2>
+                <p>
+                    ...
+                </p>
+            </section>
+            <section>
+                <h2>Experience</h2>
+                <ul>
+                    <li>...</li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default Resume;
