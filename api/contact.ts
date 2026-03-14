3891d80ee54addb633e3b1cd981fd492da68d4b8
Donovan McGilbray <djaymcgilbray@gmail.com>
2026-03-13 21:08:27 -0700

    changed email in contact.ts

--- api/contact.ts
+++ api/contact.ts
@@ -12,7 +12,7 @@ export default async function handler(req: any, res: any) {
     }
     try {
         await resend.emails.send({
-            from: "Portfolio Contact <onboarding@resend.dev>",
+            from: "Portfolio Contact <djaymcgilbray@gmail.com>",
             to: "djaymcgilbray@gmail.com",
             replyTo: email,
             subject: `New contact from ${name}`,
